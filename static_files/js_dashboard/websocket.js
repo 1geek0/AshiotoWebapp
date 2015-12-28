1491eef3147876486e5ed85f225ab5061630ff88
NilayKulkarni <typhoscientic@gmail.com>
2015-12-29 00:28:30 +0530

    Changed name

--- static_files/js_dashboard/websocket.js
+++ static_files/js_dashboard/websocket.js
@@ -1,9 +1,9 @@
 var socket = new WebSocket("ws://localhost/websock");
 
-ws.onopen = function(){
-    ws.send("Hello Ashioto!");
+socket.onopen = function(){
+    socket.send("Hello Ashioto!");
 };
 
-ws.onmessage = function(evt){
-    alert(evt.data);
+socket.onmessage = function(evt){
+    console.log(evt);
 };
