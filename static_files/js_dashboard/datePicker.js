3f8bf30a4ea1e18722dc4f236ee683e680a95640
NilayKulkarni <typhoscientic@gmail.com>
2016-02-02 00:03:58 +0530

    Handled hiding and showing of sliders, cards and datepickers

--- static_files/js_dashboard/datePicker.js
+++ static_files/js_dashboard/datePicker.js
@@ -3,5 +3,14 @@ $('.datepicker').pickadate({
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 $("#radio_time_day_one").click(function(){
-    
+    $("#date_one").show();
+    $("#card_range").hide();
+    $("#slider_overall_range").hide();
 });
+$("#radio_time_day_between").click(function(){
+    $("#date_one").attr("placeholder", "Select Start Date");
+    $("#date_one").show();
+    $("#date_two").show();
+    $("#card_range").hide();
+    $("#slider_overall_range").hide();
+})
