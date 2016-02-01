a8841aec0c6d1dae67f3ec55eacc440b5b639fa8
NilayKulkarni <typhoscientic@gmail.com>
2016-02-01 22:47:30 +0530

    Added datePicker.js for initializing and handling date picking

---
+++ static_files/js_dashboard/datePicker.js
@@ -0,0 +1,7 @@
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+$("#radio_time_day_one").click(function(){
+    
+});
