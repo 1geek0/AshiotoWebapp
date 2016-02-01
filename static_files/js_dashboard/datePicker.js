6d4a4b2787dd2ed6d880d5ce9bf2416d59089f1e
NilayKulkarni <typhoscientic@gmail.com>
2016-02-02 03:12:21 +0530

    Handled UI elements for all radio buttons

--- static_files/js_dashboard/datePicker.js
+++ static_files/js_dashboard/datePicker.js
@@ -3,7 +3,10 @@ $('.datepicker').pickadate({
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 $("#radio_time_day_one").click(function(){
+    $("#date_one").attr("placeholder", "Select Date To Show Stats Of");
     $("#date_one").show();
+    $("#date_two").hide();
+    $("#card_step").show();
     $("#card_range").hide();
     $("#slider_overall_range").hide();
 });
@@ -11,6 +14,22 @@ $("#radio_time_day_between").click(function(){
     $("#date_one").attr("placeholder", "Select Start Date");
     $("#date_one").show();
     $("#date_two").show();
+    $("#card_step").hide();
     $("#card_range").hide();
     $("#slider_overall_range").hide();
-})
+    $("#slider_overall_step").hide();
+});
+$("#radio_time_start").click(function(){
+    $("#date_one").hide();
+    $("#date_two").hide();
+    $("#card_step").show();
+    $("#card_range").show();
+    $("#slider_overall_range").show();
+});
+$("#radio_event_start").click(function(){
+    $("#date_one").hide();
+    $("#date_two").hide();
+    $("#card_step").show();
+    $("#card_range").show();
+    $("#slider_overall_range").show();
+});
