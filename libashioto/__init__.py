e1d5c404d68eee6a016b4565c3d4d50e77a32176
NilayKulkarni <typhoscientic@gmail.com>
2016-09-17 06:28:24 +0530

    Added flow_rate

--- libashioto/__init__.py
+++ libashioto/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['genmethods', 'graphmethods', 'variables', 'passmethods']
+__all__ = ['genmethods', 'graphmethods', 'variables', 'passmethods', 'flow_rate']
 from libashioto.genmethods import *
 from libashioto.variables import *
 from libashioto.graphmethods import *
 from libashioto.passmethods import *
+from libashioto.flow_rate import *
