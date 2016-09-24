bd33db0f30f815d5f120e0e3cc3d30c1efbaac06
NilayKulkarni <typhoscientic@gmail.com>
2016-09-24 12:46:45 +0530

    Added 4.5x factor

--- libashioto/genmethods.py
+++ libashioto/genmethods.py
@@ -128,7 +128,7 @@ def showRally(user, event_requested):
     realtime = events[event_requested]['realtime']
     total_count = total(all_gates)
 
-    tapovanCount = int(4.5*(all_gates[0]['count'] + all_gates[1]['count']))
+    tapovanCount = int(5*(all_gates[0]['count'] + all_gates[1]['count']))
     tapovanLatest = max(all_gates[0]['last_sync'], all_gates[1]['last_sync'])
 
     rkCount = int(4.5*all_gates[2]['count'])
