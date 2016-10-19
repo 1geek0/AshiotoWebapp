f72abafab1c5b6cf4d6794a291e558c8f0fd7237
NilayKulkarni <typhoscientic@gmail.com>
2016-10-19 13:47:56 +0530

    Restricted listEvents to return only public events

--- libashioto/genmethods.py
+++ libashioto/genmethods.py
@@ -163,7 +163,8 @@ def listEvents():
     events_db = db.ashioto_events.find({}, {"eventCode": 1, "_id": 0})
     x = 0
     for event in events_db:
-        events_list.append(event['eventCode'])
+        if event['type'] == "public":
+            events_list.append(event['eventCode'])
     events_dict['Events'] = events_list
     return events_dict
 
