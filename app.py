ef6da7073948a77cb37b48896ac0cfdbfcabed48
NilayKulkarni <typhoscientic@gmail.com>
2016-11-01 15:28:31 +0530

    Changed port to 8000

--- app.py
+++ app.py
@@ -345,6 +345,5 @@ if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(app, xheaders=True)
     #http_server.start(0)
     #http_server.bind(options.port)
-    http_server.listen(80)
-    http_server.listen(8080)
+    http_server.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
